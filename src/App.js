a07f9368d4161fe72f4d4a56c0890a8d9f97d7b0
Arup Kumar Behera <76170236+ArupGhoost@users.noreply.github.com>
2021-10-21 23:09:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { WbCloudy, WbSunny } from '@material-ui/icons';
+
+
 
 function App() {
+
+  const apiKey = 'bf6eb9b5bdd3c3e5f8c43c97205ae277';
  
   const[location, setLocation] = useState('Angul');
   const[weather, setWeather] = useState([]);
   const[name, setName] = useState([]);
   const[country, setCountry] = useState([]);
+  const[typeWeather, setTypeWeather] = useState([]);
+  
 
-   
-  const weatherApi = async () => {
-      let value = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=26bad1de49aba2243da7c50c64fb3dce`)
+   const changeLocation = (e) => {
+      let newEvent = e.target.value;
+        setLocation(newEvent);
+      }
+
+  useEffect(() => {
+    const weatherApi = async () => {
+       await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`)
+      .then((res) => res.data)
       .then((res) => {
-        let mainValue = res.data; 
+        let mainValue = res; 
        
         setWeather(mainValue.main);
         setName(mainValue.name);
         setCountry(mainValue.sys);
+        setTypeWeather(mainValue.weather['0']);
       })
       .catch((err) => {
-        console.log('Err', err)
+        console.log(err)
       })
   }
-
-   const changeLocation = (e) => {
-        setLocation(e.target.value);
-      }
-
-  useEffect(() => {
        weatherApi();
       
-  }, [location, name]);
+  }, [location]);
 
   return (
     <div className="App">
@@ -39,12 +47,30 @@ function App() {
       <h2 className='hdr1'>GhostWeatherApp</h2>
 
       <div className='main-input'> 
-         <input type='text' placeholder='Search...' id='input1' value={location} onChange={changeLocation}/>
+      <form>
+         <input type='text' placeholder='Search...' id='input1' value={location} onChange={changeLocation} />
+      </form>
       </div>
       </header>
-    { weather ? (<div className='weather-area'>
+
+
+    { !weather ? "No search"
+    :
+    (<div className='weather-area'>
             <p className='city'>{name}, {country.country}</p>
+            <div className='type-weather'>
+            {typeWeather.main === 'Clear' ?( 
+            <span>{typeWeather.main} <WbSunny className='wt'/></span>
             
+            )
+            :
+            typeWeather.main === 'Clouds' ? 
+            (   <span>{typeWeather.main} <WbCloudy className='wt'/></span>)
+            :
+            <span>{typeWeather.main}</span>
+            }
+         
+            </div>
             <div className='temps'>
             <p>Current Temp:- {weather.temp} °C</p>
             <p>Lowest Temp:- {weather.temp_min} °C</p>
@@ -52,7 +78,7 @@ function App() {
             <p>Humidity:- {weather.humidity} </p>
             <p>Pressure:- {weather.pressure} Pa</p>
             </div>
-      </div>): "No search"}
+      </div>)}
      
          
     </div>
